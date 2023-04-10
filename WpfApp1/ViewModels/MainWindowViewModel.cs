0edb442f118196f4bc704c7c71e252fd9395fb13
Ataki_pw11 <Ataki_pw11@STHQ0116-05>
2023-04-10 16:55:58 +0400

    Mvvm added

---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.TextFormatting;
+using WpfApp1.Command;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public TextBox Name { get; set; }
+        public TextBox Surname { get; set; }
+        RelayCommand SaveCommand { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            SaveCommand = new RelayCommand(obj =>
+            {
+                Surname.Text = Name.Text;
+                Name.Text = "";
+            });
+        }
+
+    }
+}
