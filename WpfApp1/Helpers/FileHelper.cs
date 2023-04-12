3be30acbc0f22497c93d983b1b5656ac0de94c74
Ataki_pw11 <Ataki_pw11@STHQ0127-07>
2023-04-12 16:15:54 +0400

    push

---
+++ WpfApp1/Helpers/FileHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using WpfApp1.Models;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace WpfApp1.Helpers
+{
+    public class FileHelper
+    {
+        public static void WriteHumans(Human users)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamWriter($"{users.Name}.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Formatting.Indented;
+                    serializer.Serialize(jw, users);
+                }
+            }
+        }
+        public static Human ReadHumans()
+        {
+            Human user = null;
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader("humans.json"))
+            {
+                using (var jr = new JsonTextReader(sr))
+                {
+                    user = serializer.Deserialize<Human>(jr);
+                }
+            }-
+            return user;
+        }
+        public static void WriteHumansXml(List<Human> Users)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Human>));
+            using (TextWriter writer = new StreamWriter("humans.xml"))
+            {
+                serializer.Serialize(writer, Users);
+            }
+        }
+        public static List<Human> ReadHumanXml()
+        {
+            List<Human> users = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Human>));
+            using (TextReader reader = new StreamReader("humans.xml"))
+            {
+                users = (List<Human>)serializer.Deserialize(reader);
+            }
+            return users;
+        }
+    }
+}
--- WpfApp1/ViewModels/MainWindowViewModel.cs
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -1,27 +1,162 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.TextFormatting;
+using System.Xml.Serialization;
 using WpfApp1.Command;
+using WpfApp1.Helpers;
+using WpfApp1.Models;
 
 namespace WpfApp1.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
-        public TextBox Name { get; set; }
-        public TextBox Surname { get; set; }
-        RelayCommand SaveCommand { get; set; }
+        interface IAdapter
+        {
+            void Write(Human user);
+            //List<Human> Read();
+        }
+        class JsonAdapter : IAdapter
+        {
+            private Json _json;
+
+            public JsonAdapter(Json json)
+            {
+                _json = json;
+            }
+
+            //public List<Human> Read()
+            //{
+            //    return _json.Read();
+            //}
+
+            public void Write(Human user)
+            {
+                _json.Write(user);
+            }
+
+     
+        }
+        class XmlAdapter : IAdapter
+        {
+            private XML _xml { get; set; }
+            public XmlAdapter(XML xml)
+            {
+                _xml = xml;
+            }
+
+            //public List<Human> Read()
+            //{
+            //    return _xml.Read();
+            //}
+
+            public void Write(Human user)
+            {
+                _xml.Write(user);
+            }
+        }
+        class Json
+        {
+            public List<Human> Read()
+            {
+
+                List<Human> users = null;
+                var serializer = new JsonSerializer();
+                using (var sr = new StreamReader("humans.json"))
+                {
+                    using (var jr = new JsonTextReader(sr))
+                    {
+                        users = serializer.Deserialize<List<Human>>(jr);
+                    }
+                }
+                return users;
+
+            }
+            public void Write(Human users)
+            {
+                FileHelper.WriteHumans(users);
+            }
+        }
+        class XML
+        {
+            public List<Human> Read()
+            {
+                List<Human> users = null;
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Human>));
+                using (TextReader reader = new StreamReader("humans.xml"))
+                {
+                    users = (List<Human>)serializer.Deserialize(reader);
+                }
+                return users;
+            }
+
+            public void Write(List<Human> users)
+            {
+                FileHelper.WriteHumansXml(users);
+            }
+        }
+        class Converter
+        {
+            private readonly IAdapter _adapter;
+            public Converter(IAdapter adapter)
+            {
+                _adapter = adapter;
+            }
+            public void Write(List<Human> users)
+            {
+                _adapter.Write(users);
+            }
+            //public List<Human> Read()
+            //{
+            //    return _adapter.Read();
+            //}
+        }
+        class Application
+        {
+            private readonly Converter _converter;
+            public Application(IAdapter adapter)
+            {
+                _converter = new Converter(adapter);
+            }
+            public void Write(List<Human> users)
+            {
+                _converter.Write(users);
+
+            }
+            //public List<Human> Read()
+            //{
+            //    return _converter.Read();
+            //}
+        }
+        public RelayCommand SaveCommand { get; set; }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; OnPropertyChanged(); }
+        }
 
 
         public MainWindowViewModel()
         {
             SaveCommand = new RelayCommand(obj =>
             {
-                Surname.Text = Name.Text;
-                Name.Text = "";
+                Name = String.Empty;
+                Surname = String.Empty;
             });
         }
 
